1b43cbd1356c6d10b267e242652c5ca333676653
Bruno <bruno.lisvoa.ext@simplit-solutions.com>
2023-11-06 16:58:25 -0300

    add yup validations

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  description: Yup.string().required("Description is required"),
+  website: Yup.string().url("Invalid URL"),
+});
 
 const PatientForm = ({ initialValues, onSubmit }) => {
   const [uploadedImage, setUploadedImage] = React.useState(null);
@@ -18,15 +25,13 @@ const PatientForm = ({ initialValues, onSubmit }) => {
   return (
     <Formik
       initialValues={initialValues}
+      validationSchema={validationSchema}
       onSubmit={(values) => onSubmit({ ...values, avatar: uploadedImage || initialValues.avatar })}
     >
       {({ isSubmitting }) => (
         <Form className="p-4 bg-white rounded shadow-md">
           <div className="mb-4">
-            <label
-              htmlFor="name"
-              className="block text-gray-700 font-bold mb-2"
-            >
+            <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
               Name
             </label>
             <Field
@@ -35,12 +40,10 @@ const PatientForm = ({ initialValues, onSubmit }) => {
               name="name"
               className="w-full px-3 py-2 placeholder-gray-400 border rounded-lg appearance-none focus:shadow-outline-blue focus:outline-none focus:ring"
             />
+            <ErrorMessage name="name" component="div" className="text-red-500" />
           </div>
           <div className="mb-4">
-            <label
-              htmlFor="description"
-              className="block text-gray-700 font-bold mb-2"
-            >
+            <label htmlFor="description" className="block text-gray-700 font-bold mb-2">
               Description
             </label>
             <Field
@@ -50,13 +53,11 @@ const PatientForm = ({ initialValues, onSubmit }) => {
               rows="4"
               className="w-full px-3 py-2 placeholder-gray-400 border rounded-lg appearance-none focus:shadow-outline-blue focus:outline-none focus:ring"
             />
+            <ErrorMessage name="description" component="div" className="text-red-500" />
           </div>
 
           <div className="mb-4">
-            <label
-              htmlFor="website"
-              className="block text-gray-700 font-bold mb-2"
-            >
+            <label htmlFor="website" className="block text-gray-700 font-bold mb-2">
               Website
             </label>
             <Field
@@ -65,13 +66,11 @@ const PatientForm = ({ initialValues, onSubmit }) => {
               name="website"
               className="w-full px-3 py-2 placeholder-gray-400 border rounded-lg appearance-none focus:shadow-outline-blue focus:outline-none focus:ring"
             />
+            <ErrorMessage name="website" component="div" className="text-red-500" />
           </div>
 
           <div className="mb-4">
-            <label
-              htmlFor="photo"
-              className="block text-gray-700 font-bold mb-2"
-            >
+            <label htmlFor="photo" className="block text-gray-700 font-bold mb-2">
               Avatar
             </label>
             <Field
@@ -81,17 +80,15 @@ const PatientForm = ({ initialValues, onSubmit }) => {
               onChange={handleImageUpload}
               className="w-full px-3 py-2 placeholder-gray-400 border rounded-lg appearance-none focus:shadow-outline-blue focus:outline-none focus:ring"
             />
+            <ErrorMessage name="photo" component="div" className="text-red-500" />
           </div>
 
           {uploadedImage && (
             <div className="mb-4">
-              <img
-                src={uploadedImage}
-                alt="Uploaded"
-                className="w-auto h-auto max-w-20 max-h-20"
-              />
+              <img src={uploadedImage} alt="Uploaded" className="w-auto h-auto max-w-20 max-h-20" />
             </div>
-          )}
+          )
+          }
 
           <div className="mt-6">
             <button
