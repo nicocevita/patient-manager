60b5a3b957c156f72da5ec3e4d0556d6972367eb
Nicolas Varela <nicolasevarela@gmail.com>
2024-02-07 19:32:26 -0300

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div class="border-gray-200 bg-gray-900 w-full">
       <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-          Patients Manager
+          Patients Managser
         </span>
         <div class="ml-5">
           <Search />
